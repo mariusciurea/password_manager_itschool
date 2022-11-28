e20f0fbed9509303f2ce421d7431afe04c4f1e8e
mariusciurea <ciurea.marius1@gmail.com>
2022-11-28 19:27:45 +0200

    app implemented without the accout_menu func

--- main.py
+++ main.py
@@ -4,13 +4,15 @@
 password manager
 """
 
-from libs import read_write
+from libs import read_write as rw
+import random
+import string
 
 
 def menu() -> str:
     print(f'save account: [s]\n'
           f'exit [e]')
-    option = input('your choise: ')
+    option: str = input('your choise: ')
     return option
 
 
@@ -18,9 +20,21 @@ def account_menu():
     pass
 
 
-def generate_random_pass():
-    pass
+def generate_random_pass() -> str:
+    p = random.sample(string.ascii_letters, 4) + random.sample(string.digits + string.punctuation, 6)
+    password = ''.join(p)
+    return password
 
 
 if __name__ == '__main__':
-    menu()
+    result = menu()
+    print(result)
+    if result == 's':
+        app, passwd = account_menu()
+        file_content: list = rw.read_from_file('apps.txt')
+        for row in file_content:
+            if app in row:
+                print('aplicatia deja exista!')
+                break
+        else:
+            rw.write_to_file('apps.txt', f'{app}:{passwd}')
